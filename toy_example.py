194a95c120ec6a6599132e08edfc09fd599963f3
muaz-asr <muhammad.muaz@dfki.de>
2024-03-28 10:44:50 +0100

    updated code

--- toy_example.py
+++ toy_example.py
@@ -30,8 +30,8 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 x_train, x_test = x_train[..., tf.newaxis], x_test[..., tf.newaxis]
 
-train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(1024)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(1024)
 
 
 class MyModel(Model):
@@ -121,10 +121,10 @@ for epoch in range(EPOCHS):
 
     template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
     print(template.format(epoch + 1,
-                          train_loss.result(),
-                          train_accuracy.result() * 100,
-                          test_loss.result(),
-                          test_accuracy.result() * 100))
+                          round(train_loss.result().numpy(), 4),
+                          round(train_accuracy.result().numpy() * 100, 1),
+                          round(test_loss.result().numpy(), 4),
+                          round(test_accuracy.result().numpy() * 100, 1)))
 
     try:
         train_loss.reset_states()
